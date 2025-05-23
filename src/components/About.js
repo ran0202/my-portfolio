9cd38e1815b7703a922d3876a055d5899cf8122a
ran0202 <ducay.ranulfo@wealthbank.com.ph>
2025-05-23 14:25:50 +0800

    added

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,14 @@ export default function About() {
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Ranulfo.
-            <br className="hidden lg:inline-block" />I love to build amazing apps.
-          </h1>
+            <br className="hidden lg:inline-block" />
+            <span className="text-xl text-gray-300">
+                I build user-focused web applications that make a real impact.
+            </span>
+        </h1>
+
           <p className="mb-8 leading-relaxed">
             I'm a front-end enthusiast exploring everything from kernel-level code to AI and blockchain. I love blending logic and creativity through code.
           </p>
