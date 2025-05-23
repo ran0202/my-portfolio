61f226fb24f4e42e6c5fb2634a47dcfeb4fe55a1
ran0202 <ducay.ranulfo@wealthbank.com.ph>
2025-05-23 11:57:02 +0800

    start

---
+++ src/data.js
@@ -0,0 +1,64 @@
+// src/data.js
+
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+  
+  // ✅ Add this:
+  export const skills = [
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Express",
+    "MongoDB",
+    "Tailwind CSS",
+    "Git & GitHub",
+    "TypeScript",
+  ];
+  export const testimonials = [
+    {
+      name: "Sarah Johnson",
+      company: "Acme Corp",
+      quote:
+        "This developer exceeded our expectations with the final product. The communication and dedication were top-notch.",
+      image: "https://randomuser.me/api/portraits/women/44.jpg",
+    },
+    {
+      name: "James Smith",
+      company: "TechNova",
+      quote:
+        "Working with them was a seamless experience. Delivered on time and provided creative solutions to our problems.",
+      image: "https://randomuser.me/api/portraits/men/46.jpg",
+    },
+  ];  
