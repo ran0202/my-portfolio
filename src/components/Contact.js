b6b7f59f7443dacddedf54ac57595096e2c09af1
ran0202 <ducay.ranulfo@wealthbank.com.ph>
2025-05-26 15:18:25 +0800

    commented function handlesubmit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,33 +8,33 @@ export default function Contact() {
   const [message, setMessage] = React.useState("");
   const [botField, setBotField] = React.useState(""); // honeypot field
 
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
+  // function encode(data) {
+  //   return Object.keys(data)
+  //     .map(
+  //       (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+  //     )
+  //     .join("&");
+  // }
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  // function handleSubmit(e) {
+  //   e.preventDefault();
 
-    // If botField is filled, do nothing (spam protection)
-    if (botField) return;
+  //   // If botField is filled, do nothing (spam protection)
+  //   if (botField) return;
 
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => {
-        alert("Message sent!");
-        setName("");
-        setEmail("");
-        setMessage("");
-      })
-      .catch((error) => alert(error));
-  }
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: encode({ "form-name": "contact", name, email, message }),
+  //   })
+  //     .then(() => {
+  //       alert("Message sent!");
+  //       setName("");
+  //       setEmail("");
+  //       setMessage("");
+  //     })
+  //     .catch((error) => alert(error));
+  // }
 
   return (
     <section id="contact" className="relative">
@@ -84,7 +84,6 @@ export default function Contact() {
           method="POST"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
-          onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <input type="hidden" name="form-name" value="contact" />
